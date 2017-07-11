cbc2e919aad352cfc1368e10706e8e1645ebbccd
coderlxf <598600855@qq.com>
2017-07-11 23:32:09 +0800

    UIView+Color

---
+++ LXFCommonCode/LXFCommonCode/CommonCode/Category/UIView/UIView+Color.h
@@ -0,0 +1,18 @@
+//
+//  UIView+Color.h
+//  LXFCommonCode
+//
+//  Created by 林洵锋 on 2017/7/11.
+//  Copyright © 2017年 LXF. All rights reserved.
+//
+//  GitHub: https://github.com/LinXunFeng
+//  简书: http://www.jianshu.com/users/31e85e7a22a2
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Color)
+
+// 获取某点的颜色
+- (UIColor *)colorOfPoint:(CGPoint)point;
+
+@end
---
+++ LXFCommonCode/LXFCommonCode/CommonCode/Category/UIView/UIView+Color.m
@@ -0,0 +1,30 @@
+//
+//  UIView+Color.m
+//  LXFCommonCode
+//
+//  Created by 林洵锋 on 2017/7/11.
+//  Copyright © 2017年 LXF. All rights reserved.
+//
+
+#import "UIView+Color.h"
+
+@implementation UIView (Color)
+
+// 获取某点的颜色
+- (UIColor *)colorOfPoint:(CGPoint)point {
+    unsigned char pixel[4] = {0};
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace, kCGImageAlphaPremultipliedLast);
+    CGContextTranslateCTM(context, -point.x, -point.y);
+    
+    [self.layer renderInContext:context];
+    
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    
+    UIColor *color = [UIColor colorWithRed:pixel[0]/255.0 green:pixel[1]/255.0 blue:pixel[2]/255.0 alpha:pixel[3]/255.0];
+    
+    return color;
+}
+
+@end
